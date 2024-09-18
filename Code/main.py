8becf1248eca637568691450b8e49335e39f7536
Timothy Riddle <tim.riddle.tr@gmail.com>
2024-09-18 13:23:49 -0600

    Loaded Images/ Sounds/ Music

--- Code/main.py
+++ Code/main.py
@@ -16,15 +16,27 @@ clock = pygame.time.Clock()
 #Set Game Values
 
 #Set Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 #Set Fonts
+font = pygame.font.Font(join("Assets", "Franxurter.ttf"))
 
 #Set text
 
-#Set Sound and Music
+#Load Sound and Music
+clicker_sound = pygame.mixer.Sound(join("Assets", "click_sound.wav"))
+miss_sound = pygame.mixer.Sound(join("Assets", "miss_sound.wav"))
+background_music = pygame.mixer.music.load(join("Assets", "ctc_background_music.wav"))
 
-#Set Images
+#Load Images
+background_image = pygame.image.load(join("Assets", "background.png")).convert_alpha()
+background_image_rect = background_image.get_rect()
+background_image_rect.center = (WIDTH, HEIGHT)
 
+clown_image = pygame.image.load(join("Assets", "clown.png")).convert_alpha()
+clown_image_rect = clown_image.get_rect()
+clown_image_rect.center = (0,0)
 
 #Main game loop
 running = True
