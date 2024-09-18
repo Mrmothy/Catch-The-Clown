6f60e3b535b7f1af324ffe4e6d264fa85444f7c3
Timothy Riddle <tim.riddle.tr@gmail.com>
2024-09-18 09:51:11 -0600

    Added in the rest of what is needed for the game assets.

--- Code/main.py
+++ Code/main.py
@@ -9,6 +9,22 @@ WIDTH, HEIGHT = 945, 600
 display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Catch The Clown")
 
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+#Set Game Values
+
+#Set Colors
+
+#Set Fonts
+
+#Set text
+
+#Set Sound and Music
+
+#Set Images
+
 
 #Main game loop
 running = True
